b180b29657275ab486ae73e64a775a23fee5f374
maxeleron <maxeleron@gmail.com>
2016-07-22 19:39:32 +0300

    Shortcut icon & js_attemps
    - Added shortcut icon (for future);
    - Added js code (program doesn`t work).

--- MyPC_components_(table)/js/calc.js
+++ MyPC_components_(table)/js/calc.js
@@ -1,2 +1,12 @@
-alert("Hello");
+var a = 2.6 + 1.29 + 0.885 + 4.768 + 4.435 + 1.64 + 0.785 + 1.2 + 1 + 4.6 + 0.9 + 0.24;
 
+var text = "price12121price";
+var regExp1 = /price/;
+
+
+alert(a);
+var table1 = document.getElementsByTagName('table');
+
+
+var result = table1.search(regExp1);
+alert( result );
