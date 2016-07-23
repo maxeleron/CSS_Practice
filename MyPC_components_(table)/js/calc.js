8f2db240949cd3ded28ad1fd3c413a22b0c80d09
maxeleron <maxeleron@gmail.com>
2016-07-23 19:03:35 +0300

    Summ <td>
    - I fill summ <td> (calculating by myself, without js).

--- MyPC_components_(table)/js/calc.js
+++ MyPC_components_(table)/js/calc.js
@@ -1,10 +1,13 @@
-var a = 2.6 + 1.29 + 0.885 + 4.768 + 4.435 + 1.64 + 0.785 + 1.2 + 1 + 4.6 + 0.9 + 0.24;
+var summ = 2.6 + 1.29 + 0.885 + 4.768 + 4.435 + 1.64 + 0.785 + 1.2 + 1 + 4.6 + 0.9 + 0.24;
+/*var summP = 2600 + 1290 + 885 + 4768 + 4435 + 1640 + 785 + 1200 + 1000 + 4600 + 900 + 240;*/
+
+summ = 24.342999999999993;
+/*summP = 24343;*/
 
 var text = "price12121price";
 var regExp1 = /price/;
-
-
-alert(a);
+document.write(summP);
+/*document.write(summ);*/
 var table1 = document.getElementsByTagName('table');
 
 
