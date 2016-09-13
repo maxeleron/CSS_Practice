2d5695af30d889cf36d475c7d6ca417c68d1bc8c
maxeleron <maxeleron@gmail.com>
2016-09-13 20:17:00 +0300

    SummCalc is fully completed
    - SummCalc js-function works;
    - Now all files translated to English.

--- MyPC_components_(table)/js/calc.js
+++ MyPC_components_(table)/js/calc.js
@@ -1,15 +1,19 @@
-var summ = 2.6 + 1.29 + 0.885 + 4.768 + 4.435 + 1.64 + 0.785 + 1.2 + 1 + 4.6 + 0.9 + 0.24;
-/*var summP = 2600 + 1290 + 885 + 4768 + 4435 + 1640 + 785 + 1200 + 1000 + 4600 + 900 + 240;*/
+calcSumm = function (prices){
+	var summ = 0;
+	for (var i = prices.length - 1; i >= 0; i--) {
+		summ += parseFloat(prices[i].innerText);
+	}
+	//I know that I am a wheel creator, but now I don`t have better solution)))
+	summ *= 1000;
+	summ = parseInt(summ);
+	summ = parseFloat(summ);
+	summ /= 1000;
+	var currentSumm = prices = document.getElementsByClassName("sumCalc");
+	currentSumm[0].innerText = summ;
 
-summ = 24.342999999999993;
-/*summP = 24343;*/
+}
 
-var text = "price12121price";
-var regExp1 = /price/;
-document.write(summP);
-/*document.write(summ);*/
-var table1 = document.getElementsByTagName('table');
+var prices = document.getElementsByClassName("price");
+calcSumm(prices);
 
 
-var result = table1.search(regExp1);
-alert( result );
