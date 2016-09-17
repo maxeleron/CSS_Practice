8c65f5ff1791b8c4887215d0b0244ca4f205b778
maxeleron <maxeleron@gmail.com>
2016-09-17 12:15:18 +0300

    MyPC summ calculation fixed.
    - Using number method .toFixed(x) instead a "wheel".

--- MyPC_components_(table)/js/calc.js
+++ MyPC_components_(table)/js/calc.js
@@ -3,13 +3,9 @@ calcSumm = function (prices){
 	for (var i = prices.length - 1; i >= 0; i--) {
 		summ += parseFloat(prices[i].innerText);
 	}
-	//I know that I am a wheel creator, but now I don`t have better solution)))
-	summ *= 1000;
-	summ = parseInt(summ);
-	summ = parseFloat(summ);
-	summ /= 1000;
-	var currentSumm = prices = document.getElementsByClassName("sumCalc");
-	currentSumm[0].innerText = summ;
+
+	var currentSumm = document.getElementsByClassName("sumCalc");
+	currentSumm[0].innerText = summ.toFixed(3);
 
 }
 
